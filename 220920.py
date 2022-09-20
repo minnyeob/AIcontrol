3d1e98c18eb21ac0d862024236e1d8d3b268ba65
shitasd <slgkf654@naver.com>
2022-09-20 16:31:06 +0900

    프로그램이전

---
+++ 220920.py
@@ -0,0 +1,153 @@
+# # # print ("Hello World")
+# # # print ("김민엽")
+# ////////////////////////////
+# # #TRUE = 1
+
+# # #if TRUE : 
+# # #    print("나는")
+# # #    print("바보입니다.")
+# # #    print("True")
+
+# # #else :
+# # #    print("그런데")
+# # #    print("바보와 천재는 종이 한장차이입니다")
+# # #    print("False")
+# ///////////////////////////
+# # counter = 100
+# # miles = 1000.0
+# # name = "John"
+
+# # print(counter)
+# # print(miles)
+# # print(name)
+
+# //////////////////////////
+# # var1 = 1
+# # var2 = 10
+
+# # print(var1)
+# # print(var2)
+
+#////////////////////////////////
+# str = 'Hello World'
+
+# print (str)
+# print (str[0])
+# print (str[2:5])
+# print (str[2:])
+# print (str*2)
+# print (str+"test")
+
+#//////////////////////////
+#매우 중요 list형, 나열형, 열거형
+#대괄호는 list
+
+#!/usr/bin/python3
+
+# list = ['abcd', 786, 2.23, 'john', 70.2]
+# tinylist = [123, 'john']
+
+# print (list)
+# print(list[0])
+# print(list[1:3])
+# print(list[2:])
+# print(tinylist * 2)
+# print(list + tinylist)
+
+# /////////////////////
+
+# tuples 웹앱, 금융, 보안
+# 데이터가 바뀌면 안되는거
+# 한번쓰면 수정불가
+# 소괄호
+
+# tuple = ('abcd', 786 , 2.23, 'john', 70.2)
+# tinytuple = (123, 'john')
+
+# print(tuple)
+# print(tuple[0])
+# print(tuple[1:3])
+# print(tuple[2:])
+# print(tinytuple * 2)
+# print(tuple + tinytuple)
+
+# tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
+# list = [ 'abcd', 786 , 2.23, 'john', 70.2  ]
+# tuple[2] = 1000    
+# list[2] = 1000 
+
+# //////////////////////////////
+
+# dictionary 사전형
+# 중괄호 key : value 형태를 가지는 집합
+
+# dict = {}
+# dict['one'] = "this is one"
+# dict[2] = "this is two"
+
+# tinydict = {'name' : 'john', 'code' : 6734, 'dept' : 'sales'}
+
+# print(dict['one'])
+# print(dict[2])
+# print(tinydict)
+# print(tinydict.keys())
+# print(tinydict.values())\
+
+# //////////////////////////////////////
+
+# 과제
+# one = 1 
+# >>> '1'
+
+# two = 2;
+# >>> '2'
+# >>> one + two = 계산불가
+
+# 3이 나오려하면 one을 숫자형으로 변환
+# >> int(one,10) > 10진수로 바꿈
+
+# >>> one = int(one,10)
+# >>> one + two
+# >>> 3
+
+# type conversion 시트를 마크다운파일로 생성
+
+# jupyter
+
+# overvie
+# enviroment setup
+# ㄴ 설치
+
+# basic syntax
+# ~ variable Types 기본문법
+
+# decision Making 조건
+
+# loops 반복
+
+# numbers~date&time 변수의 확장버전
+
+# 개발 방법
+# function
+# modules
+# exceptions
+# classes
+
+# multithreading  < case studying
+
+# xvl processing < 데이터
+
+# cgi~email < 웹기술
+
+# powershell, cmd 쉘 프로그램
+# cmd > python > 파이선 들어가짐
+
+# <<< = 파이선쉘
+
+# 터미널 > 새 터미널 > 
+
+# stdio 표준입출려환경
+
+# cd. /파일명\
+
+# #!/usr/bin/python3를 맨처음에 입력
